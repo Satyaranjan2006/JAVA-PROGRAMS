40fe93354d029f68034caea71731d79b4096edc7
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-08-31 23:54:56 +0530

    binary to Decimal

---
+++ BASIC/binaryToDecimal.java
@@ -0,0 +1,28 @@
+package BASIC;
+import java.util.Scanner;
+class Binary_Decimal
+ {
+     Scanner scan;
+     int num;
+     void getVal() 
+      {
+                  System.out.println("Binary to Decimal");
+   	 scan = new Scanner(System.in);
+	 System.out.println("\nEnter the number :");
+	 num = Integer.parseInt(scan.nextLine(), 2);
+      }
+    void convert() 
+      {
+                  String decimal = Integer.toString(num);
+   	System.out.println("Decimal Value is : " + decimal);
+      }
+}
+
+public class binaryToDecimal {
+    public static void main(String args[]) 
+      {
+          Binary_Decimal obj = new Binary_Decimal();
+           obj.getVal();
+           obj.convert();
+      }
+}
