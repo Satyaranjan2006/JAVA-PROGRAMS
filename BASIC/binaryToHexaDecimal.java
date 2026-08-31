e7b5b8f1a9830e922ba90f51f0f1f7ffc09f7cb8
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-08-31 23:58:44 +0530

    binary to hexadecimal

---
+++ BASIC/binaryToHexaDecimal.java
@@ -0,0 +1,29 @@
+package BASIC;
+import java.util.Scanner;
+
+public class binaryToHexaDecimal {
+    public static void main(String... q) 
+            {
+	Binary_Hexa obj = new Binary_Hexa();
+	obj.getVal();
+	obj.convert();
+              }
+}
+
+class Binary_Hexa 
+   {
+                 Scanner scan;
+	int num;
+	void getVal() 
+	    {
+		System.out.println("Binary to HexaDecimal");
+		scan = new Scanner(System.in);
+		System.out.println("\nEnter the number :");
+		num = Integer.parseInt(scan.nextLine(), 2);
+	    }
+	void convert() 
+	   {
+		String hexa = Integer.toHexString(num);
+		System.out.println("HexaDecimal Value is : " + hexa);
+	   }
+    }
