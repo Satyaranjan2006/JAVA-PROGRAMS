9e4f1228a8d9e618ab7e684bf3c34954306e1e9c
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-05 23:59:51 +0530

    update

--- SpecialProgram/getApi.java
+++ SpecialProgram/getApi.java
@@ -9,3 +9,15 @@ public class getApi {
         System.out.println("IP of my system is := "+IP.getHostAddress());
     }    
 }
+
+// //package SpecialProgram;
+
+// import java.net.InetAddress;
+
+// public class getApi {
+//     public static void main(String args[]) throws Exception
+//     {
+//         InetAddress IP = InetAddress.getLocalHost();
+//         System.out.println("IP of my system is := "+IP.getHostAddress());
+//     }    
+// }
