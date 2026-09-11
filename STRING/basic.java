de1a24a862acedd1045934558e7fbd30fe1c433b
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-11 23:53:08 +0530

    reverse string

---
+++ STRING/basic.java
@@ -0,0 +1,8 @@
+package STRING;
+
+public class basic {
+    public static void main(String[] args) {
+        char ch='A';
+        System.out.println(Character.toLowerCase(ch));
+    }
+}
---
+++ STRING/reverse.java
@@ -0,0 +1,14 @@
+
+package STRING;
+
+public class reverse {
+    public static void main(String[] args) {
+         String original = "Hello World";
+        
+        // Pass the string to StringBuilder and call reverse()
+        String reversed = new StringBuilder(original).reverse().toString();
+        
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
