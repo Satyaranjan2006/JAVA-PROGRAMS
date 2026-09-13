bb86b6ee97fa0f29417f8fdfe0eb033d104cfbab
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-13 23:58:59 +0530

    adding some new concept

--- STRING/reverse.java
+++ STRING/reverse.java
@@ -12,3 +12,20 @@ public class reverse {
         System.out.println("Reversed: " + reversed);
     }
 }
+
+
+
+// package STRING;
+
+// public class reverse {
+//     public static void main(String[] args) {
+//          String original = "Hello World";
+        
+//         // Pass the string to StringBuilder and call reverse()
+//         String reversed = new StringBuilder(original).reverse().toString();
+        
+//         System.out.println("Original: " + original);
+//         System.out.println("Reversed: " + reversed);
+//     }
+// }
+
