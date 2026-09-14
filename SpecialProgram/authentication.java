91cdb23b60f7d91e68c7d582b72fe9ef9c958bea
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-14 23:31:42 +0530

    authentication.java

---
+++ SpecialProgram/authentication.java
@@ -0,0 +1,23 @@
+package SpecialProgram;
+
+import java.util.Scanner;
+
+public class authentication {
+     public static void main(String args[])
+    {
+        String username, password;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter username:");//username:user
+        username = s.nextLine();
+        System.out.print("Enter password:");//password:user
+        password = s.nextLine();
+        if(username.equals("user") && password.equals("user"))
+        {
+            System.out.println("Authentication Successful");
+        }
+        else
+        {
+            System.out.println("Authentication Failed");
+        }
+    }
+}
